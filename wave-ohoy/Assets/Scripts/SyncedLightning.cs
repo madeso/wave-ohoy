5f4856433212cdeb61eb8fd7d85c400031c76fc6
Gustav <sir.gustav.the.coder@gmail.com>
2017-01-22 00:58:11 +0100

    added thunder thingy

---
+++ wave-ohoy/Assets/Scripts/SyncedLightning.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncedLightning : MonoBehaviour {
+	public MusicParser Music;
+	public MusicParser.Note.Type Note;
+
+	private Light light;
+
+	void Start () {
+		this.light = this.GetComponent<Light>();
+		if( this.light == null ) {
+			Debug.Log("Failed to find the light");
+		}
+	}
+
+	float effect = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		this.effect -= Time.deltaTime;
+		if( this.effect < 0 ) this.effect = 0;
+
+		if( this.Music != null ) {
+			foreach(var n in this.Music.CurrentNotes) {
+				if( n.Event == this.Note) {
+					this.effect = 1;
+				}
+			}
+		}
+
+		if( this.light != null ) {
+			this.light.enabled = this.effect > 0.0f;
+			this.light.intensity = this.effect;
+		}
+	}
+}
