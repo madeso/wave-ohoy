e528adac2506643dcffd43b40d2f9839e39f5aea
Gustav <sir.gustav.the.coder@gmail.com>
2017-01-21 13:44:17 +0100

    fixed bug: remove wave at start

--- wave-ohoy/Assets/Scripts/KillOutside.cs
+++ wave-ohoy/Assets/Scripts/KillOutside.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 // kills the game object if it goes outside the view
 public class KillOutside : MonoBehaviour {
-	Renderer r;
+	private Renderer r;
+	private int frameIgnores = 0;
 
 	void Start() {
 		r = GetComponentInChildren<Renderer>();
@@ -13,6 +14,12 @@ public class KillOutside : MonoBehaviour {
 		}
 	}
 	void Update () {
+		// Update might be called before isVisible is updated, and hence we remove the GO before we have had
+		// the time to render it, so let's just keep the GO alive for a few updates before we remove it
+		if( this.frameIgnores < 3 ) {
+			this.frameIgnores++;
+			return;
+		}
 		if( r == null ) return;
 		if( r.isVisible==false )
 		{
