75dcd7e3c8e12a5c0bcb8be7cd472443cef6fffd
Hussein Taher <hussein_taher@outlook.com>
2017-01-22 11:08:35 +0100

    THE ACTUAL GAME

---
+++ wave-ohoy/Assets/barrelSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrelSpawn : MonoBehaviour {
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        transform.position = new Vector3(0f, Random.value * 200f + 50f, 4.5f);
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector3.zero;
+    }
+
+}
